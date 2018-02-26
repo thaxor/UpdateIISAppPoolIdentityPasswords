da629e1829f31cdfab9556a38ac21ffd0f1c9313
Luke Hansen <lukehansen@gmail.com>
2018-02-26 15:45:07 -0800

    initial checkin

---
+++ Update IIS Identity.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module WebAdministration
+
+$user = Read-host "Username"
+$pass = Read-host "Password" 
+
+$applicationPools = Get-ChildItem IIS:\AppPools | where { $_.processModel.userName -ieq $user }
+
+foreach($appPool in $applicationPools)
+{
+    $appPool | Set-ItemProperty -Name processModel.userName -Value $user
+    $appPool | Set-ItemProperty -Name processModel.password -Value $pass
+    $appPool | Set-ItemProperty -Name processModel.identityType -Value 3
+	
+	Restart-WebAppPool $appPool.name
+	
+    Write-Host "IIS:\AppPools\$($appPool.name) --- Updated!" -ForegroundColor Green
+}
+
+Write-Host "All Application pool passwords were updated..." -ForegroundColor Magenta 
+Write-Host ""
